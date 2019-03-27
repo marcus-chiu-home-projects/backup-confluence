8068c279b668f261641c938ba518a3390d9e4f5e
Marcus Chiu <marcuschiu9@gmail.com>
2019-03-26 23:39:27 -0500

    initial commit

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+rsync -avzh /var/atlassian/application-data/confluence/backups $SCRIPT_PATH/
