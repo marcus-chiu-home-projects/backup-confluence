a018e838d9585747cd0ed8a6ecf43fb43063338e
Marcus Chiu <marcuschiu9@gmail.com>
2019-11-21 17:56:27 -0600

    modified .gitignore

--- backup.sh
+++ backup.sh
@@ -5,4 +5,8 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
+printf "\n\nSyncing contents in /var/atlassian/application-data/confluence/backups to local\n"
+
 rsync -avzh /var/atlassian/application-data/confluence/backups $SCRIPT_PATH/
+
+$SCRIPT_PATH/backup-to-s3-bucket.sh
